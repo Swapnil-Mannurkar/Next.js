0c3efa4a1ca605389d7c6b66be128e45cf973db6
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-11-23 14:34:41 +0530

    SSG on all events page

--- event-management-app-2/pages/events/index.js
+++ event-management-app-2/pages/events/index.js
@@ -1,12 +1,11 @@
 import EventList from "@/components/events/EventList";
 import EventsSearch from "@/components/events/EventsSearch";
-import { getAllEvents } from "@/dummy-data";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 
-const AllEventsPage = () => {
+const AllEventsPage = (props) => {
+  const events = props.allEvents;
   const router = useRouter();
-  const events = getAllEvents();
 
   const findEventsHandler = (year, month) => {
     const pathname = `/events/${year}/${month}`;
@@ -19,6 +18,24 @@ const AllEventsPage = () => {
       <EventList items={events} />;
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const response = await fetch(
+    "https://nextjs-course-3d59e-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+  const allEvents = [];
+
+  for (const key in data) {
+    allEvents.push(data[key]);
+  }
+
+  return {
+    props: {
+      allEvents: allEvents,
+    },
+  };
 };
 
 export default AllEventsPage;
