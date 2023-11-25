da6950cf1973b19f834a0915279edcbdb099d5ba
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-11-25 13:02:00 +0530

    dynamic api route

--- api-routes/pages/api/[feedbackId].js
+++ api-routes/pages/api/[feedbackId].js
@@ -3,10 +3,9 @@ import { getFileData } from "./feedback";
 const handler = (req, res) => {
   const feedbackId = req.query.feedbackId;
   const data = getFileData();
-  const selectedFeedback = data.find((feedback) => (feedback.id = feedbackId));
+  const selectedFeedback = data.find((feedback) => feedback.id === feedbackId);
 
   res.status(200).json({ feedback: selectedFeedback });
 };
-
 
 export default handler;
--- api-routes/pages/feedback/index.js
+++ api-routes/pages/feedback/index.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { getFileData } from "../api/feedback";
 
 const FeedbackPage = (props) => {
+  const [feedbackData, setFeedbackData] = useState();
+
   const loadSelectedFeedbackHandler = (id) => {
-    fetch(`/api/${id}`);
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => setFeedbackData(data.feedback));
   };
 
   return (
-    <ul>
-      {props.feedback.map((item) => (
-        <li key={item.id}>
-          {item.feedback}{" "}
-          <button onClick={loadSelectedFeedbackHandler.bind(null, item.id)}>
-            Show Details
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedback.map((item) => (
+          <li key={item.id}>
+            {item.feedback}{" "}
+            <button onClick={loadSelectedFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
