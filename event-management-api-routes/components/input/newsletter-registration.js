36b207ede35ef79e125c027f7ae698e225995139
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-11-26 00:41:54 +0530

    added registration api

--- event-management-api-routes/components/input/newsletter-registration.js
+++ event-management-api-routes/components/input/newsletter-registration.js
@@ -1,12 +1,20 @@
-import classes from './newsletter-registration.module.css';
+import { useRef } from "react";
+import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
+  const emailRef = useRef();
+
   function registrationHandler(event) {
     event.preventDefault();
+    const enteredEmail = emailRef.current.value;
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    fetch("/api/newsletter", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: enteredEmail }),
+    });
   }
 
   return (
@@ -15,10 +23,11 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            ref={emailRef}
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
           />
           <button>Register</button>
         </div>
