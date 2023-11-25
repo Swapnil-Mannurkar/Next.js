a68c1e937b0e48f2789776dd4bb4a07de849706e
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-11-25 12:36:11 +0530

    dynamic routing

---
+++ api-routes/pages/api/[feedbackId].js
@@ -0,0 +1,11 @@
+import { getFileData } from "./feedback";
+
+const handler = (req, res) => {
+  const feedbackId = req.query.feedbackId;
+  const data = getFileData();
+  const selectedFeedback = data.find((feedback) => (feedback.id = feedbackId));
+
+  res.status(200).json({ feedback: selectedFeedback });
+};
+
+export default handler;
--- api-routes/pages/feedback/index.js
+++ api-routes/pages/feedback/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import { getFileData } from "../api/feedback";
 
 const FeedbackPage = (props) => {
+  const loadSelectedFeedbackHandler = (id) => {
+    fetch(`/api/${id}`);
+  };
+
   return (
     <ul>
       {props.feedback.map((item) => (
-        <li key={item.id}>{item.feedback}</li>
+        <li key={item.id}>
+          {item.feedback}{" "}
+          <button onClick={loadSelectedFeedbackHandler.bind(null, item.id)}>
+            Show Details
+          </button>
+        </li>
       ))}
     </ul>
   );
