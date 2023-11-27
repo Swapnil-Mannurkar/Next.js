6dcaecce29356e96af4dcb3a5c5a79b77a31b2f6
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-11-27 18:33:48 +0530

    addded main navigation

---
+++ blog-website/components/layout/Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Logo.module.css";
+
+const Logo = () => {
+  return <div className={styles.logo}>Swapnil Mannurkar</div>;
+};
+
+export default Logo;
