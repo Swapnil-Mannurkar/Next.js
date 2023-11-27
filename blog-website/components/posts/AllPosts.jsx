2e91579b452a16ecde1609d1674cc09d7171ee7b
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-11-27 19:42:15 +0530

    added all posts page

---
+++ blog-website/components/posts/AllPosts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./AllPosts.module.css";
+import PostsGrid from "./PostsGrids";
+
+const AllPosts = (props) => {
+  return (
+    <section className={styles.posts}>
+      <h1>All Posts</h1>
+      <PostsGrid posts={props.posts} />
+    </section>
+  );
+};
+
+export default AllPosts;
--- blog-website/pages/posts/index.js
+++ blog-website/pages/posts/index.js
@@ -1,7 +1,39 @@
 import React from "react";
+import AllPosts from "../../components/posts/AllPosts";
 
 const AllPostPage = () => {
-  return <div>AllPostPage</div>;
+  const DUMMY_DATA = [
+    {
+      slug: "getting-started-with-NextJS",
+      title: "Getting started with NextJS",
+      image: "getting-started-with-nextjs.png",
+      date: "2022-02-10",
+      excerpt: "NextJS is a React framework for production",
+    },
+    {
+      slug: "getting-started-with-NextJS2",
+      title: "Getting started with NextJS",
+      image: "getting-started-with-nextjs.png",
+      date: "2022-02-10",
+      excerpt: "NextJS is a React framework for production",
+    },
+    {
+      slug: "getting-started-with-NextJS3",
+      title: "Getting started with NextJS",
+      image: "getting-started-with-nextjs.png",
+      date: "2022-02-10",
+      excerpt: "NextJS is a React framework for production",
+    },
+    {
+      slug: "getting-started-with-NextJS4",
+      title: "Getting started with NextJS",
+      image: "getting-started-with-nextjs.png",
+      date: "2022-02-10",
+      excerpt: "NextJS is a React framework for production",
+    },
+  ];
+
+  return <AllPosts posts={DUMMY_DATA} />;
 };
 
 export default AllPostPage;
