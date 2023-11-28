b7d11ac7ac43c2a1cccb2f8db718a032390cf9e1
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-11-28 19:35:21 +0530

    Processing API UI

--- blog-website/components/contact/ContactForm.jsx
+++ blog-website/components/contact/ContactForm.jsx
@@ -1,27 +1,66 @@
 import React from "react";
 import styles from "./ContactForm.module.css";
-import { useRef } from "react";
 import { useState } from "react";
+import Notification from "../ui/notification";
 
 const ContactForm = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
+  const [notification, setNotification] = useState({
+    title: "",
+    message: "",
+    status: "",
+  });
+  const [displayNotification, setDisplayNotification] = useState(false);
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
+    setDisplayNotification(true);
     e.preventDefault();
-
-    fetch(`/api/contact`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        name,
-        message,
-      }),
+    setNotification({
+      title: "Processing...",
+      message: "Sending your message!",
+      status: "pending",
     });
+
+    try {
+      const response = await fetch(`/api/contact`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          name,
+          message,
+        }),
+      });
+      const data = await response.json();
+
+      if (response.ok) {
+        setNotification({
+          title: "Successful",
+          message: "Your message has been sent!",
+          status: "success",
+        });
+
+        setTimeout(() => {
+          setDisplayNotification(false);
+        }, 2000);
+      } else {
+        throw new Error(data.message || "Something went wrong!");
+      }
+    } catch (error) {
+      setNotification({
+        title: "Error",
+        message: error.message || "Something went wrong!",
+        status: "error",
+      });
+
+      setTimeout(() => {
+        setDisplayNotification(false);
+      }, 2000);
+    }
   };
 
   return (
@@ -64,6 +103,7 @@ const ContactForm = () => {
           </div>
         </div>
       </form>
+      {displayNotification && <Notification notification={notification} />}
     </section>
   );
 };
