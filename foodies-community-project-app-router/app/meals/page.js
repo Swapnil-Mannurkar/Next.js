809a9efb434e5b8887af8f8b5818f112200c2acd
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-07 22:47:40 +0530

    added meals page structure

--- foodies-community-project-app-router/app/meals/page.js
+++ foodies-community-project-app-router/app/meals/page.js
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 
 const MealsPage = () => {
-  return <div>MealsPage</div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you!</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
