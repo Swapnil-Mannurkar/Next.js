70acd8a5b0e90c822edbc12c2998845b8ebf117b
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-07 15:42:46 +0530

    added header

---
+++ foodies-community-project-app-router/components/main-header/main-header.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+import logoImg from "@/assets/logo.png";
+import styles from "./main-header.module.css";
+import Image from "next/image";
+
+const MainHeader = () => {
+  return (
+    <header className={styles.header}>
+      <Link href={"/"} className={styles.logo}>
+        <Image src={logoImg} alt="logo" priority />
+      </Link>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href={"/meals"}>Browse Meals</Link>
+          </li>
+          <li>
+            <Link href={"/community"}>Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
