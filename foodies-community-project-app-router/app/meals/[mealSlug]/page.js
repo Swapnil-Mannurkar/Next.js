6320ab9a94cbeb5a098ffb485158bd0c272481d4
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-08 11:51:43 +0530

    added meal page

---
+++ foodies-community-project-app-router/app/meals/[mealSlug]/page.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+
+const MealsSlugPage = ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+
+  const instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={styles.heaederText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: instructions }}
+        ></p>
+      </main>
+    </>
+  );
+};
+
+export default MealsSlugPage;
